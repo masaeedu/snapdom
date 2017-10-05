7c107b2b57a3cc4ff104485319bd28b96ac74d16
Asad Saeeduddin <masaeedu@gmail.com>
2017-10-04 21:14:41 -0400

    Refactor type declarations

--- src/stateManagers.ts
+++ src/stateManagers.ts
@@ -3,7 +3,7 @@ import { StateManager } from "./types";
 type Synchronous = <TRoot extends { [P in TPath]: any }, TPath extends string>(
   state: TRoot,
   path: TPath
-) => StateManager<TRoot[TPath]>;
+) => StateManager<TRoot[TPath], (next: TRoot[TPath]) => void>;
 export const synchronous: Synchronous = (state, path) => {
   const subscribers: any[] = [];
   return {
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,16 @@
 export type Component<TState, TVDom> = (
   set: Function
 ) => (state: TState) => TVDom;
-export type StateManager<TState> = {
-  get: (listener: (state: TState) => void) => void;
-  set: (state: TState) => void;
+
+export type Get<TState> = (listener: (state: TState) => void) => void;
+
+export type StateManager<TState, TSet extends Function = Function> = {
+  get: Get<TState>;
+  set: TSet;
 };
+
 export type Target<TVDom> = (vdom: TVDom) => void;
+
 export type Snap = <TState, TVDom>(
   cmp: Component<TState, TVDom>
 ) => (stateManager: StateManager<TState>) => (target: Target<TVDom>) => void;
